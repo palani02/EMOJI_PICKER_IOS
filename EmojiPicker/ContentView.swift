ee813caccceedb7d29ebbcf113fb4392f9dc72a8
palani02 <palanibaba2002@gmail.com>
2023-10-21 21:13:49 +0530

    Initial Commit for Emoji Picker

--- EmojiPicker/ContentView.swift
+++ EmojiPicker/ContentView.swift
@@ -7,15 +7,33 @@
 
 import SwiftUI
 
+// Make enum to store different type of case value.
+enum Emoji:String,CaseIterable{
+    case 👻,👽,🤪,🤟🏻,🥷🏻,🤖,😵,🥶,😎,🤫,👾,😼
+}
+
 struct ContentView: View {
+    // use state anotation. Because i need to change
+    // character every time
+    @State var Selection:Emoji = .👾
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                Text(Selection.rawValue)
+                    .font(.system(size: 200))
+                
+                Picker("Choose the Emoji to Display", selection:$Selection){
+                    // anotation = $
+                    ForEach(Emoji.allCases,id:\.self){
+                        // id should be unique. So i give their value itself
+                        Emoji in Text(Emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.wheel)
+            }
+            .padding()
+            .navigationTitle("Emoji Picker !!!")
         }
-        .padding()
     }
 }
 
