182918bb2f2b5c0db87026ca5caab523480c3b17
PalaniBaba <palanibaba2002@gmail.com>
2023-10-21 13:52:34 +0530

    Initial Commit

---
+++ EmojiPicker/EmojiPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerApp.swift
+//  EmojiPicker
+//
+//  Created by Palani on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
